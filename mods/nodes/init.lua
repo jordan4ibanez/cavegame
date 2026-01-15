39c8cce226a7d96443d92d1e42782673410b730e
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-14 20:27:36 -0500

    Start with a nice way to automate namespace

--- mods/nodes/init.lua
+++ mods/nodes/init.lua
@@ -1,1 +1,10 @@
 -- Entry point into what makes up the world.
+
+infdev = infdev or {}
+
+--- Custom node registration.
+---@param name string
+---@param definition table
+function infdev.register_node(name, definition)
+	core.register_node(":infdev:" .. name, definition)
+end
