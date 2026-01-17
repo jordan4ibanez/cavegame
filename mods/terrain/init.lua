cae176057922dcb6fe8084d0512da11e05326242
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-16 20:39:54 -0500

    Add better load up for the terrain mod

--- mods/terrain/init.lua
+++ mods/terrain/init.lua
@@ -3,6 +3,10 @@
 core.set_mapgen_setting("mg_name", "singlenode", true)
 core.set_mapgen_setting("mg_flags", "nolight", true)
 
+local mod_path = core.get_modpath(core.get_current_modname())
+
+dofile(mod_path .. "/ore.lua")
+
 core.register_mapgen_script(
-	core.get_modpath(core.get_current_modname()) .. "/mapgen.lua"
+	mod_path .. "/mapgen.lua"
 )
