bf32cf5b8c7a5c5bf6d6f15774caf9287ff82ad0
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2026-01-16 21:04:16 -0500

    Add coal and iron ore

--- mods/terrain/ore.lua
+++ mods/terrain/ore.lua
@@ -1,1 +1,39 @@
 -- Ores generated in the world.
+
+infdev = infdev or {}
+
+local stone_disabled = true
+
+function infdev.register_ore(def)
+	if (stone_disabled) then
+		def.wherein = "air"
+	end
+
+	-- Automate cluster scarcity. Typing that 3 times is annoying.
+	-- Average distance between ore deposits.
+	def.clust_scarcity = math.pow(def.clust_scarcity, 3)
+
+	core.register_ore(def)
+end
+
+infdev.register_ore({
+	ore_type       = "scatter",
+	ore            = "infdev:coal",
+	wherein        = "infdev:stone",
+	clust_scarcity = 14,
+	clust_num_ores = 8,
+	clust_size     = 3,
+	y_max          = 160,
+	y_min          = -1023,
+})
+
+infdev.register_ore({
+	ore_type       = "scatter",
+	ore            = "infdev:iron",
+	wherein        = "infdev:stone",
+	clust_scarcity = 14,
+	clust_num_ores = 8,
+	clust_size     = 3,
+	y_max          = 160,
+	y_min          = -1023,
+})
